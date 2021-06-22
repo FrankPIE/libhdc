03ab2fb9ca3c28ce622f7387731f10be1ad41617
裴博翔 <peiboxiang@ndengine.com>
2021-06-22 11:43:48 +0800

    update example

--- example/main.c
+++ example/main.c
@@ -10,15 +10,10 @@
 
 int main(int argc, char* argv[])
 {
-	//hdc_smbios_table_data_t* data;
-
 	hdc_handle_t* handle;
 
 	hdc_init(&handle);
 	
-	//printf("Base Board : %s ", hdc_smbios_baseboard_manufacturer(data));
-	//printf("%s\n", hdc_smbios_baseboard_product(data));
-
 	unsigned char uuid[16];
 	hdc_system_uuid(handle, uuid);
 
@@ -29,10 +24,6 @@ int main(int argc, char* argv[])
 		uuid[8], uuid[9], 
 		uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]
 	);
-
-	//printf("BIOS : %s ", hdc_smbios_bios_vendor(data));
-	//printf("%s\n", hdc_smbios_bios_version(data));
-	//printf("Release Date : %s\n", hdc_smbios_bios_release_date(data));
 
 	hdc_destroy(handle);
 	
